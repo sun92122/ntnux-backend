70c89a6ba4e638f8460bc2bd6cd2cafbbd6443a7
magical <sun920122@gmail.com>
2025-04-13 02:57:41 +0800

    🚧 嘗試格式化腳本

--- tool/stript.py
+++ tool/stript.py
@@ -5,7 +5,7 @@ import os
 CHUNK_SIZE = 100
 
 
-def parse_course(original_data_path: str, output_dir: str, output_file_prefix: str = "courses_"):
+def parse_course(original_data_path: str):
     with open(original_data_path, "r", encoding="utf-8") as f:
         raw_blocks = json.load(f)
 
@@ -31,20 +31,24 @@ def parse_course(original_data_path: str, output_dir: str, output_file_prefix: s
                 all_courses.append(course)
 
     print(f"✔️ 共保留課程數：{len(all_courses)}")
+    return all_courses
 
+
+def strip_course(courses_list: list[dict], output_dir: str, output_file_prefix: str = "courses_"):
     # === 輸出分段檔案 ===
     os.makedirs(output_dir, exist_ok=True)
 
-    for i in range(0, len(all_courses), CHUNK_SIZE):
-        chunk = all_courses[i:i+CHUNK_SIZE]
+    for i in range(0, len(courses_list), CHUNK_SIZE):
+        chunk = courses_list[i:i+CHUNK_SIZE]
         path = os.path.join(
             output_dir, f"{output_file_prefix}{i//CHUNK_SIZE+1}.min.json")
         with open(path, "w", encoding="utf-8") as out:
             json.dump(chunk, out, ensure_ascii=False, separators=(",", ":"))
 
-    file_num = (len(all_courses)-1)//CHUNK_SIZE+1
+    file_num = (len(courses_list)-1)//CHUNK_SIZE+1
     with open(os.path.join(output_dir, f"{output_file_prefix}0.min.json"), "w", encoding="utf-8") as out:
-        json.dump({"total": file_num}, out, ensure_ascii=False, separators=(",", ":"))
+        json.dump({"total": file_num}, out,
+                  ensure_ascii=False, separators=(",", ":"))
     print(f"✅ 完成輸出：{file_num} 個檔案 → {output_dir}")
 
 
@@ -53,11 +57,15 @@ def main():
         os.path.dirname(__file__), "../data/response_content.json"))
     output_dir = os.path.abspath(os.path.join(
         os.path.dirname(__file__), "../../frontend/public/data"))
-    output_prefix = "112-1_"
+    output_prefix = "113-2_"
     if not os.path.exists(original_data_path):
         print(f"⚠️ 檔案不存在：{original_data_path}")
         return
-    parse_course(original_data_path, output_dir, output_prefix)
+    # courses = parse_course(original_data_path)
+    with open(original_data_path, "r", encoding="utf-8") as f:
+        courses = json.load(f)
+    strip_course(courses, output_dir, output_prefix)
+    print(f"✔️ 完成處理：{len(courses)} 筆課程資料")
 
 
 if __name__ == "__main__":
